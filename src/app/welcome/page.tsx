15dac61eee71a702bd3d14e711eca3b30af2ce34
tomohiro11 <b1700430@gl.aiu.ac.jp>
2025-06-01 16:21:53 +0900

    add: add buttons to the welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,3 +1,22 @@
+import Button from '@/ui/button';
+
 export default function Welcome() {
-  return <div>Welcome to Xitter</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="max-w-5xl w-full font-mono text-sm">
+        <h1 className="text-4xl font-bold mb-8 text-center">
+          Welcome to Xitter
+        </h1>
+        
+        <div className="flex flex-col gap-4 items-center"> 
+          <Button>
+            Create Account
+          </Button>
+          <Button>
+            Login
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
