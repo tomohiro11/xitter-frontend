b7c3e2a2d3d5e8a709f58acea9ed7a76208726f1
tomohiro11 <b1700430@gl.aiu.ac.jp>
2025-06-01 16:20:57 +0900

    update: make the buttons have the same widths

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -10,7 +10,7 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className="group rounded-lg border-2 border-gray-300 px-5 py-4 transition-colors hover:border-gray-400 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      className="group rounded-lg border-2 border-gray-300 px-5 py-4 transition-colors hover:border-gray-400 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-48 text-center"
       {...props}
     >
       {children}
