de99db5054886a6e48e80924ad5085f7379b77a3
tomohiro11 <b1700430@gl.aiu.ac.jp>
2025-06-01 16:12:35 +0900

    add: create a button component

---
+++ src/ui/button.tsx
@@ -0,0 +1,19 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+export default function Button({
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className="group rounded-lg border-2 border-gray-300 px-5 py-4 transition-colors hover:border-gray-400 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
